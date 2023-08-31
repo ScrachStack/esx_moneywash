3173aa1dde05eb028acc957bff1b9e65d9d22315
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-30 17:59:47 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+-- JOIN FOR SUPPORT https://discord.gg/cfxdev
+Config = {
+    Locations = {
+        { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 10 }, -- this is the location from the one on the fourms post!
+        { name = "Location 2", x = 1234.56, y = -789.01, z = 32.10, tax = 15 },
+        { name = "Location 3", x = 987.65, y = -543.21, z = 12.34, tax = 20 },
+    },
+}
+
+Config.Misc = {
+    UseprogressCircle = false,
+    Progressduration = 2000,
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+-- JOIN FOR SUPPORT https://discord.gg/cfxdev
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'zaps6000'
+description '[ESX]Money Wash Script'
+version '1.0.0'
+
+server_scripts { 
+    "server/*",
+}
+
+client_scripts { 
+    "client/*",
+}
+
+shared_scripts { '@es_extended/imports.lua', 'config.lua', '@ox_lib/init.lua'
+}
+
+
